adda74cb2687de6682a2c2047d8a3ff6e2e233dc
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2016-07-05 11:24:30 +0200

    Commands statistics

--- src/Core/Commander/Providers/CacheRessource.js
+++ src/Core/Commander/Providers/CacheRessource.js
@@ -4,6 +4,8 @@
  * Description: Cette classe singleton est un cache des ressources et services
  */
 
+ var __DEV__ = true;
+
 var instanceCache = null;
 
 function CacheRessource() {
@@ -12,19 +14,30 @@ function CacheRessource() {
     this.cacheObjects = [];
     this._maximumSize = null;
 
+    if (__DEV__) {
+        this.statistics = { hit: 0, miss: 0, count: 0};
+    }
 }
 
 /**
  * @param url
  */
 CacheRessource.prototype.getRessource = function(url) {
-
+    if (__DEV__) {
+        if (url in this.cacheObjects) {
+            this.statistics.hit++;
+        } else {
+            this.statistics.miss++;
+        }
+    }
     return this.cacheObjects[url];
 
 };
 
 CacheRessource.prototype.addRessource = function(url, ressource) {
-
+    if (__DEV__) {
+        this.statistics.count++;
+    }
     this.cacheObjects[url] = ressource;
 
 };
